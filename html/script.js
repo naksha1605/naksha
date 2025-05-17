b683e182cbd92d7bfea897b46667b14bc64c2eab
naksha <naksha1603@gmail.com>
2025-05-17 10:26:25 +0530

    day 3

---
+++ html/script.js
@@ -0,0 +1,23 @@
+var a = 5;
+var b = 8;
+console.log(a)
+console.log(b)
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a < b)
+console.log(a > b)
+console.log(a = b)
+function add() {
+    var a = 5;
+    var b = 10;
+    console.log(a + b)
+}
+add()
+function add(a, b) {
+    console.log(a + b)
+}
+add(20, 30)
+add(50, 30)
+add(10, 30)
