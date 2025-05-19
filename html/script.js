cc6561cbd806f0c44135552aa120063426fc4481
naksha <naksha1603@gmail.com>
2025-05-19 10:22:18 +0530

    4day

--- html/script.js
+++ html/script.js
@@ -21,3 +21,35 @@ function add(a, b) {
 add(20, 30)
 add(50, 30)
 add(10, 30)
+var a = 5
+var b = '5'
+console.log(a == b)
+
+var a = 29;
+var b = 30;
+console.log(`${a} +${b} = ${a + b}`)
+n = 45
+if (n % 2 == 0) {
+    console.log('even')
+}
+else {
+    console.log('odd')
+}
+for (n = 1; n <= 1; n++) {
+    if (n % 2 == 0) {
+        console.log(`${n}is even`)
+    }
+
+}
+n = 10
+var result = (n % 2 === 0) ? "even" : "odd"
+console.log(result)
+n = 10;
+sum = 1;
+if (n % 2 == 0) {
+    console.log(`${sum +
+        
+        
+        n}is even`)
+
+}
